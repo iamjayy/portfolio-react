cede310117b5b97d9bdca18495fc2470b603547e
jason <jason_doan94@yahoo.com>
2020-04-10 17:29:24 -0400

    create files

---
+++ portfolio/src/Components/Banner/Banner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return <div>About Me</div>;
+};
+
+export default Banner;
---
+++ portfolio/src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return <div>About Me</div>;
+};
+
+export default LandingPage;
